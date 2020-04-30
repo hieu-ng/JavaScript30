4260fa72637452968b5caebead5a63637b681d82
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-01 09:57:02 +1000

    Add globalCompositeOperation

--- 08 - Fun with HTML5 Canvas/app.js
+++ 08 - Fun with HTML5 Canvas/app.js
@@ -6,6 +6,7 @@ ctx.strokestyle = "#BADASS";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
