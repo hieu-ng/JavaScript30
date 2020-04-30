7e7ed801c93bc49d041b97a1028664c7e7fc27bc
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-01 09:38:32 +1000

    Setting up canvas

---
+++ 08 - Fun with HTML5 Canvas/app.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokestyle = "#BADASS";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
