42c7adf3e5174d471628966c3681963eaf965ce4
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-04 09:53:15 +1000

    Fixing scrub functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -48,4 +48,9 @@ ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
     range.addEventListener("mousemove", handleRangeUpdate)
 );
+
+let mousedown = false;
 progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
