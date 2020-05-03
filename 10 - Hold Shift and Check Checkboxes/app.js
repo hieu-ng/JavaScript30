df9ce5873eb76320262fabedbe3bea37bb327af3
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-03 19:17:50 +1000

    Adding function handleCheck

--- 10 - Hold Shift and Check Checkboxes/app.js
+++ 10 - Hold Shift and Check Checkboxes/app.js
@@ -1,5 +1,11 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
+function handleCheck(e) {
+    lastChecked = this;
+}
+
 checkboxes.forEach((checkbox) =>
     checkbox.addEventListener("click", handleCheck)
 );
