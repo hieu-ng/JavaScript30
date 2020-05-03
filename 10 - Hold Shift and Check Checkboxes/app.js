85759ebbeeecc6fcdac2c69dc808ae14630b9a97
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-03 19:22:32 +1000

    Check in between boxes

--- 10 - Hold Shift and Check Checkboxes/app.js
+++ 10 - Hold Shift and Check Checkboxes/app.js
@@ -4,6 +4,16 @@ let lastChecked;
 
 function handleCheck(e) {
     lastChecked = this;
+    //Check if they have the Shift key down
+    //Check if they are checking a box
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach((checkbox) => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+        });
+    }
 }
 
 checkboxes.forEach((checkbox) =>
