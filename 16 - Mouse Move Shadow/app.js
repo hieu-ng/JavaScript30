ac1af7e113ea97d8bd8d4a37a99a6f1257e7ba53
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-09 10:56:28 +1000

    Adding style when moving mouse in

--- 16 - Mouse Move Shadow/app.js
+++ 16 - Mouse Move Shadow/app.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100;
 
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;
@@ -9,6 +10,11 @@ function shadow(e) {
         x = x + e.target.offsetLeft;
         y = y + e.target.offsetTop;
     }
+
+    const xWalk = Math.round((x / width) * walk - walk / 2);
+    const yWalk = Math.round((y / height) * walk - walk / 2);
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
 }
 
 hero.addEventListener('mousemove', shadow);
