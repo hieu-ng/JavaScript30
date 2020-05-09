97409883cd11168d657f0eaadf7fb6e8acaadf1e
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-09 10:52:14 +1000

    Adding adjustments to offset X and offset Y

--- 16 - Mouse Move Shadow/app.js
+++ 16 - Mouse Move Shadow/app.js
@@ -4,6 +4,11 @@ const text = hero.querySelector('h1');
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;
     let { offsetX: X, offsetY: y } = e;
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
 }
 
 hero.addEventListener('mousemove', shadow);
