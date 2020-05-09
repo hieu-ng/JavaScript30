666172b819ba1b22ff469a64d2ce23101aa0fdff
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-09 10:50:23 +1000

    Adding shadow function to listen to mousemove event

--- 16 - Mouse Move Shadow/app.js
+++ 16 - Mouse Move Shadow/app.js
@@ -1,6 +1,9 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
-function shadow(e) {}
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: X, offsetY: y } = e;
+}
 
 hero.addEventListener('mousemove', shadow);
