a225f58754e50d70f3ada77ca2febbfe378614fe
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-13 09:25:10 +1000

    Append transcript to HTML

--- 20 - Speech Detection/app.js
+++ 20 - Speech Detection/app.js
@@ -13,6 +13,13 @@ recognition.addEventListener('result', (e) => {
         .map((result) => result[0])
         .map((result) => result.transcript)
         .join('');
+
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
 });
 
+recognition.addEventListener('end', recognition.start);
 recognition.start();
