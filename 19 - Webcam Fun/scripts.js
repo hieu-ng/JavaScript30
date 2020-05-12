5803419c49a17addac2b35f9c07c15efe44d8bec
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-12 19:19:50 +1000

    Fixing redEffect bug

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,6 +26,7 @@ function paintToCanvas() {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
         pixels = redEffect(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -45,11 +46,12 @@ function takePhoto() {
 }
 
 function redEffect() {
-    for (let i = 0; i < pixels.length; i += 4) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100;
         pixels.data[i + 1] = pixels.data[i + 1] - 50;
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
     }
+    return pixels;
 }
 
 getVideo();
