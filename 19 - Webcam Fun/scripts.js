a0d1606989d01067c8fe55c181c11650e2086b41
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-12 18:57:28 +1000

    Add take photo feature to takePhoto

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,8 +28,18 @@ function paintToCanvas() {
 }
 
 function takePhoto() {
+    // Play the sound
     snap.currentTime = 0;
     snap.play();
+
+    // Take data out of canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
+video.addEventListener('canplay', paintToCanvas);
