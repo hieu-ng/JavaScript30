1cf787bd38aa047a6a9ac13cf2cb868d19f0b2f1
Hieu Nguyen <hieutnguyen06@gmail.com>
2020-05-13 09:17:24 +1000

    Installing node modules

--- 20 - Speech Detection/app.js
+++ 20 - Speech Detection/app.js
@@ -1,2 +1,13 @@
 window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('results', (e) => {
+    console.log(e);
+});
