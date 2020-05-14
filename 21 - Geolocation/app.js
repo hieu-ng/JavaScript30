c9f7608ed178d522c91a631f88f5aa594c5b073d
hieu-ng <hieutnguyen06@gmail.com>
2020-05-14 18:21:58 +1000

    Rotate the compass

--- 21 - Geolocation/app.js
+++ 21 - Geolocation/app.js
@@ -3,4 +3,5 @@ const speed = document.querySelector(".speed-value");
 
 navigator.geolocation.watchPosition((data) => {
     speed.textContent = Math.round(data.coords.speed);
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 });
