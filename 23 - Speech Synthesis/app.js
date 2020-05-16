8b436776b3033e3c509adf8d34b4865c062c7a0e
hieu-ng <hieutnguyen06@gmail.com>
2020-05-16 10:27:15 +1000

    Find voices by their names

--- 23 - Speech Synthesis/app.js
+++ 23 - Speech Synthesis/app.js
@@ -2,18 +2,24 @@ const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector("#speak");
-const stopButton = document.querySelector("#stop");
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
 
 msg.text = document.querySelector('[name="text"]').value;
 
 function populateVoices() {
     voices = this.getVoices();
-    voiceDropdown.innerHTML = voices
+    voicesDropdown.innerHTML = voices
         .map(
             (voice) =>
                 `<option value="${voice.name}">${voice.name} (${voice.lang})`
         )
-        .join("");
+        .join('');
 }
-speechSynthesis.addEventListener("voiceschanged", populateVoices);
+
+function setVoice() {
+    msg.voice = voices.find((voice) => voice.name === this.value);
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
