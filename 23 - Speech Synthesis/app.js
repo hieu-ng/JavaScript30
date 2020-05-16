17c29ca4d6152798fd69425c6417a623517bd710
hieu-ng <hieutnguyen06@gmail.com>
2020-05-16 10:23:35 +1000

    Adding dropdown option to select voices (Depends on system, Mac has more built-in voices)

--- 23 - Speech Synthesis/app.js
+++ 23 - Speech Synthesis/app.js
@@ -6,3 +6,14 @@ const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
 
 msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+    voices = this.getVoices();
+    voiceDropdown.innerHTML = voices
+        .map(
+            (voice) =>
+                `<option value="${voice.name}">${voice.name} (${voice.lang})`
+        )
+        .join("");
+}
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
