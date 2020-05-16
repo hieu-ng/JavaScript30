b69933eec2dabbd71baa877bc44362a00ccc708c
hieu-ng <hieutnguyen06@gmail.com>
2020-05-16 10:29:53 +1000

    MInor adjustments

--- 23 - Speech Synthesis/app.js
+++ 23 - Speech Synthesis/app.js
@@ -22,9 +22,11 @@ function setVoice() {
     toggle();
 }
 
-function toggle() {
+function toggle(startOver = true) {
     speechSynthesis.cancel();
-    speechSynthesis.speak(msg);
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
