b15c090fa8160be8b631558c0bb54b449f4db950
hieu-ng <hieutnguyen06@gmail.com>
2020-05-17 10:37:54 +1000

    Offseting nav for smooth scrolling

--- 24 - Sticky Nav/app.js
+++ 24 - Sticky Nav/app.js
@@ -3,6 +3,7 @@ const topOfNav = nav.offsetTop;
 
 function fixNav() {
     if (topOfNav <= window.scrollY) {
+        document.body.style.paddingTop = nav.offsetHeight;
         document.body.classList.add('fixed-nav');
     } else {
         document.body.classList.remove('fixed-nav');
