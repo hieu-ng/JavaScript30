911f17d084e99e7aa307dc6e5078d72e87dfe179
hieu-ng <hieutnguyen06@gmail.com>
2020-05-17 10:38:55 +1000

    Removing offsetHeight when scrolling down to the end for smooth scrolling

--- 24 - Sticky Nav/app.js
+++ 24 - Sticky Nav/app.js
@@ -3,9 +3,10 @@ const topOfNav = nav.offsetTop;
 
 function fixNav() {
     if (topOfNav <= window.scrollY) {
-        document.body.style.paddingTop = nav.offsetHeight;
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
         document.body.classList.add('fixed-nav');
     } else {
+        document.body.style.paddingTop = 0;
         document.body.classList.remove('fixed-nav');
     }
 }
