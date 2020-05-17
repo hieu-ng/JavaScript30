503e07706d1d3ab473628f6d9a7928fe272f2724
hieu-ng <hieutnguyen06@gmail.com>
2020-05-17 10:35:59 +1000

    Add style to fixed-nav

--- 24 - Sticky Nav/app.js
+++ 24 - Sticky Nav/app.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector('#main');
 const topOfNav = nav.offsetTop;
 
-function fixNav() {}
+function fixNav() {
+    if (topOfNav <= window.scrollY) {
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+    }
+}
 
 window.addEventListener('scroll', fixNav);
