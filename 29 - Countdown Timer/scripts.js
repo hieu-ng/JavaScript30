b579262a4dea38cc9b7bf1c1270fc69283fa9cb8
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 09:14:48 +1000

    Set timer function

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,4 +1,13 @@
 function timer(seconds) {
 	const now = Date.now();
-	const then = now + (seconds * 1000);
+	const then = now + seconds * 1000;
+
+	setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		// Check if we should stop - when timer goes negative!
+		if (secondsLeft < 0) {
+			clearInterval(countdown);
+			return;
+		}
+	}, 1000);
 }
