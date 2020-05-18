b1315df6000237193675bca6e13430f6011ea76c
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 09:37:20 +1000

    Clear current timer when clicking to a new timer

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -4,6 +4,9 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+	// Clear exising timer
+	clearInterval(countdown);
+
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(secondsLeft);
