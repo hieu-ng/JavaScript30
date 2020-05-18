24a0509a13f5437c148750ebb390efdef4eb2f06
hieu-ng <hieutnguyen06@gmail.com>
2020-05-18 10:01:28 +1000

    Fixing syntax error (adding commas)

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -2,13 +2,13 @@ const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelectorAll('dropdownBackground');
 const nav = document.querySelector('.top');
 
-function handleEnter(params) {
+function handleEnter(params) {}
 
-}
+function handleLeave(params) {}
 
-function handleLeave(params) {
-
-}
-
-triggers.forEach(trigger => trigger.addEventListener('mouseenter' handleEnter));
-triggers.forEach(trigger => trigger.addEventListener('mouseleave' handleLeave));
+triggers.forEach((trigger) =>
+	trigger.addEventListener('mouseenter', handleEnter)
+);
+triggers.forEach((trigger) =>
+	trigger.addEventListener('mouseleave', handleLeave)
+);
