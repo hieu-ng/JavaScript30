c7a597b7b5a885c285d566511c75ba134772833c
hieu-ng <hieutnguyen06@gmail.com>
2020-05-18 22:59:21 +1000

    Add functions to listen to mouse events on sliders

--- 27 - Click and Drag/app.js
+++ 27 - Click and Drag/app.js
@@ -2,3 +2,16 @@ const slider = document.querySelector('.items');
 let isDown = false;
 let startX;
 let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+	isDown = true;
+});
+slider.addEventListener('mouseleave', () => {
+	idDown = false;
+});
+slider.addEventListener('mouseup', () => {
+	idDown = false;
+});
+slider.addEventListener('mousemove', () => {
+	console.log(isDown);
+});
