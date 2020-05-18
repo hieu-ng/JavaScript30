6311bb5dcb079cb23b75d2c94e792284f2e1942d
hieu-ng <hieutnguyen06@gmail.com>
2020-05-18 23:01:01 +1000

    Add active class when mousedown and remove active class when mouseup

--- 27 - Click and Drag/app.js
+++ 27 - Click and Drag/app.js
@@ -5,13 +5,17 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', () => {
 	isDown = true;
+	slider.classList.add('.active');
 });
 slider.addEventListener('mouseleave', () => {
 	idDown = false;
+	slider.classList.remove('.active');
 });
 slider.addEventListener('mouseup', () => {
 	idDown = false;
+	slider.classList.remove('.active');
 });
 slider.addEventListener('mousemove', () => {
+	if (!isDown) return;
 	console.log(isDown);
 });
