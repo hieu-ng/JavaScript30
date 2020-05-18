a4a7f522a6365f7eed19b06f820cb3809a6b9c4b
hieu-ng <hieutnguyen06@gmail.com>
2020-05-18 10:06:59 +1000

    Animating nav bar when mouse enter & leave

--- 26 - Stripe Follow Along Nav/app.js
+++ 26 - Stripe Follow Along Nav/app.js
@@ -2,9 +2,14 @@ const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelectorAll('dropdownBackground');
 const nav = document.querySelector('.top');
 
-function handleEnter(params) {}
+function handleEnter(params) {
+	this.classList.add('trigger-enter');
+	setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+}
 
-function handleLeave(params) {}
+function handleLeave(params) {
+	this.classList.add('trigger-enter', 'trigger-enter-active');
+}
 
 triggers.forEach((trigger) =>
 	trigger.addEventListener('mouseenter', handleEnter)
