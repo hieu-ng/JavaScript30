89b94735432184980976ccaab8752d1cb2c40127
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 10:07:24 +1000

    Prevent popping up in the same destination

--- 30 - Whack A Mole/app.js
+++ 30 - Whack A Mole/app.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
 function randTime(min, max) {
 	return Math.round(Math.random() * (max - min) + min);
@@ -9,4 +10,9 @@ function randTime(min, max) {
 function randomeHole(holes) {
 	const idx = Math.floor(Math.random() * holes.length);
 	const hole = holes[idx];
+	if (hole === lastHole) {
+		randomeHole(holes);
+	}
+
+	lastHole = hole;
 }
