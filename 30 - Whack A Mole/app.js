1262342fbf37ddafb940f14b23cdb21cc2fa3c11
hieu-ng <hieutnguyen06@gmail.com>
2020-05-19 10:01:00 +1000

    Add random timer

--- 30 - Whack A Mole/app.js
+++ 30 - Whack A Mole/app.js
@@ -1,3 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+function randTime(min, max) {
+	return Math.random() * (max - min) + min;
+}
